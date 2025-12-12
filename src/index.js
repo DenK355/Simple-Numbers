7e82a07543947b9f6de23a68fad46597d909d13e
Denis <bezsaluy@gmail.com>
2025-12-12 13:33:42 +0200

    Task JS complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 'use strict'
+
+console.log("В задание написано от 1 до 100, но 1 это не простое число, но я его выведу таким образом: ");
+
+console.log("1 is prime")
+
+for (let num = 2; num <= 100; num++) {
+    let isPrime = true;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${num} is prime`);
+    }
+}
+    
