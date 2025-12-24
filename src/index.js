688cb9bb394090575a1fcef5371791aff592ab79
Denis <bezsaluy@gmail.com>
2025-12-24 16:58:57 +0200

    fix mistake

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 'use strict'
 
-console.log("В задание написано от 1 до 100, но 1 это не простое число, но я его выведу таким образом: ");
+let num = 100;
 
-console.log("1 is prime")
+outer:for (let i = 2; i <= num; i++) { 
 
-for (let num = 2; num <= 100; num++) {
-    let isPrime = true;
-
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${num} is prime`);
-    }
-}
+  for (let j = 2; j < i; j++) {
     
+    if (i % j == 0){
+        continue outer;
+    }
+
+  }
+
+  console.log(`${i} is prime`); 
+
+}
